df66a01686e894916513ff3901d90761c88b225b
nadjazahharovaee <alienlamahope@gmail.com>
2021-12-09 11:14:45 +0200

    Added monolog

--- public/index.php
+++ public/index.php
@@ -1,2 +1,18 @@
 <?php
+require_once('../vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('log/my.log', Logger::WARNING));
+
+$log->warning('Foo');
+$log->error('Bar');
+
+$log->warning('Memory 1 '.memory_get_usage());
+
 phpinfo();
+
+$log->warning('Memory 2 '.memory_get_usage());
